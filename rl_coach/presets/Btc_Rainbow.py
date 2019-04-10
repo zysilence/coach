5490d3893012a8f7e260949e6cd0d4fb71fcd1f6
zysilence <zysilence329b@gmail.com>
2019-04-10 11:58:23 +0800

    rainbow: use e-greedy exploration

--- rl_coach/presets/Btc_Rainbow.py
+++ rl_coach/presets/Btc_Rainbow.py
@@ -3,6 +3,7 @@ from rl_coach.architectures.tensorflow_components.layers import Conv2d, Dense
 from rl_coach.base_parameters import VisualizationParameters, PresetValidationParameters, EmbedderScheme
 from rl_coach.core_types import EnvironmentEpisodes, EnvironmentSteps
 from rl_coach.environments.gym_environment import GymEnvironmentParameters, ObservationSpaceType
+from rl_coach.exploration_policies.e_greedy import EGreedyParameters
 from rl_coach.filters.filter import NoOutputFilter, InputFilter
 from rl_coach.filters.reward.reward_clipping_filter import RewardClippingFilter
 from rl_coach.graph_managers.basic_rl_graph_manager import BasicRLGraphManager
@@ -29,6 +30,9 @@ agent_params.algorithm.num_steps_between_copying_online_weights_to_target = Envi
 agent_params.memory.max_size = (MemoryGranularity.Transitions, 100000)  # default: 1000000
 agent_params.memory.beta = LinearSchedule(0.4, 1, 12500000)  # 12.5M training iterations = 50M steps = 200M frames
 agent_params.memory.alpha = 0.5
+agent_params.exploration = EGreedyParameters()
+agent_params.exploration.epsilon_schedule = LinearSchedule(1, 0.1, 10000000)  # decault value: 1000000
+agent_params.exploration.evaluation_epsilon = 0
 agent_params.network_wrappers['main'].input_embedders_parameters['observation'].scheme = \
     [
         Conv2d(32, [5, 1], [2, 1]),
